c34359229661f46f074d9c28535e051c5cfd3666
karthik G <raams.karthik@gmail.com>
2018-01-31 20:41:20 +0530

    first upload

---
+++ real_time_object_detection.py
@@ -0,0 +1,96 @@
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", default="MobileNetSSD_deploy.prototxt.txt",
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", default="MobileNetSSD_deploy.caffemodel",
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels of MobileNet SSD 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+
+print("[INFO] starting video stream...")
+cap = cv2.VideoCapture(0)
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+
+	ret,frame = cap.read()
+	if not ret:
+		continue	
+
+	frame = imutils.resize(frame, width=400)
+
+	# grab the frame dimensions and convert it to a blob....change dimensions and check for frame rate try 200 instead of 300 and maybe 150 too
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	# pass the blob through the network and obtain the detections and predictions
+    
+	net.setInput(blob)
+    
+	detections = net.forward()
+
+
+	# loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence 
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# extract (x,y) for bounding boxes
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cap.release()
+cv2.destroyAllWindows()
